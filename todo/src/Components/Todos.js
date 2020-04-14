a05bca5d2d852e4d7e1ec646618b4410690c5ac9
Gordon Caister <gordoncaister@gmail.com>
2020-04-14 08:57:26 -0500

    got toggle action to work, Behind The Scenes

--- todo/src/Components/Todos.js
+++ todo/src/Components/Todos.js
@@ -4,13 +4,18 @@ export  const Todos = (props) => {
     const {todoList, dispatchTodoList} = props
     const handleToggle = (e) => {
         e.preventDefault();
+        console.log("e",e.target)
+        dispatchTodoList({type:'toggle', payload: e.target.className})
     }
     console.log(todoList)
     return (
         <div>
             {todoList.map(todo => {
                 return(
-                <h3>{todo.item}</h3>
+                    <div  key={todo.id} onClick={handleToggle} >
+                        <h3 className={todo.id}>{todo.item}</h3>
+                    </div>
+                
             )})}
         </div>
     )
--- todo/src/Reducers/Reducer.js
+++ todo/src/Reducers/Reducer.js
@@ -1,7 +1,20 @@
 export const reducer = function (state, action) {
-    return[
-        
-    ]
+    switch (action.type){
+        case 'toggle':
+            console.log("clicked toggle")
+            return state.map(todo => {
+                console.log(action)
+                console.log("id",todo.id);
+                console.log("payload: ", action.payload);
+                if(todo.id.toString() === action.payload) {
+                    console.log("todo is now completed")
+                    return {...todo, completed: !todo.completed}
+                }
+                return todo;
+            })
+        case 'clear':
+            return;
+    }
 }
 
 export const initialState = [{
