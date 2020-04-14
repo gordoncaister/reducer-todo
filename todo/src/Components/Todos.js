6065efac9c33939f0f67fa4b8e65b8caecde075b
Gordon Caister <gordoncaister@gmail.com>
2020-04-14 09:02:18 -0500

    got toggle to work on the front end

--- todo/src/Components/Todos.js
+++ todo/src/Components/Todos.js
@@ -13,7 +13,10 @@ export  const Todos = (props) => {
             {todoList.map(todo => {
                 return(
                     <div  key={todo.id} onClick={handleToggle} >
-                        <h3 className={todo.id}>{todo.item}</h3>
+                        <h3 
+                        className={todo.id}
+                        style={{textDecoration: `${todo.completed? 'line-through': 'none'}`}}
+                        >{todo.item}</h3>
                     </div>
                 
             )})}
